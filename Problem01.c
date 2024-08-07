39e16a38c6e2131d1762915f48bbe251911e5e03
itsnotTalha <talhajubayer737@gmail.com>
2024-08-07 21:57:48 +0600

    Here we go again

--- Problem01.c
+++ Problem01.c
@@ -1,15 +1,25 @@
 /******************************************* 
-Intro
+WAP that will take n integer numbers into an array, and then print all the integers into
+reverse order (from the last valid index to index 0).
 _____               ______
 INPUT               OUTPUT
 -----               ------
-
+5                   5 4 3 2 1
+1 2 3 4 5
 ********************************************/
 #include<stdio.h>
 int main(){
     int n;
     printf("Enter term value:");
     scanf("%d",&n);
+    int num[n];
+        printf("Enter your number:\n");
+    for(int i=0;i<n;i++){
+        scanf("%d", &num[i]);
+    }
+    printf("Reversed number:\n");
+    for(int i=n-1;i>=0;i--)
+    printf("%d ",num[i]);
 }
 /******************************************* 
  * Author- Talha Jubayer
