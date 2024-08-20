0f23fb5ccf62230f9d19d1399adac77c9d79b39f
itsnotTalha <talhajubayer737@gmail.com>
2024-08-20 06:48:01 +0600

    Use alt+x to get output in vsc

--- Problem14.c
+++ Problem14.c
@@ -4,30 +4,45 @@ swap all elements between array A and B. Finally show all elements of both array
 _____               ______
 INPUT               OUTPUT
 -----               ------
-
+8                   Array A : 3 2 1
+7 8 1 3 2 6 4 3     Array B : 7 8 1 3 2 6 4 3
+3
+3 2 1
 ********************************************/
 #include<stdio.h>
 int main(){
     int n;
-    printf("Enter Array-1 size:");
+    printf("Enter Array-A size: ");
     scanf("%d",&n);
     int num1[n];
+    int tmp[n];
     printf("Enter your numbers:\n");
     for(int i=0;i<n;i++){
         scanf("%d", &num1[i]);
+        tmp[i]=num1[i];
     }
     int m;
-    printf("Enter Array-2 size:");
+    printf("Enter Array-B size: ");
     scanf("%d",&m);
     int num2[m];
     printf("Enter your numbers:\n");
     for(int i=0;i<n;i++){
         scanf("%d", &num2[i]);
     }
-    int temp1[m];
+    //First swap
     for(int i=0;i<m;i++)
-    temp[i]=num2[i];
-    printf("%d ",temp[i]);
+        num1[i]=num2[i];
+    //Second swap
+    for(int i=0;i<n;i++)
+        num2[i]=tmp[i];
+        
+    printf("Array A: ");
+    for(int i=0;i<m;i++)
+    printf("%d ",num1[i]);
+    puts("");
+    printf("Array B: ");
+    for(int i=0;i<n;i++)
+    printf("%d ",num2[i]);
 }
 /******************************************* 
  * Author- Talha Jubayer
